ea7c808200fb531e6a87813378da5a58e52d40b0
phil-hardwick <phil_hardwick@hotmail.co.uk>
2016-06-06 09:22:21 +0100

    swagger docs change location of json file

--- swagger-docs/run.sh
+++ swagger-docs/run.sh
@@ -8,8 +8,8 @@ if [ "$API_KEY" != "**None**" ]; then
   sed -i "s|myApiKeyXXXX123456789|$API_KEY|g" index.html
 fi
 
-if [[ -f $SWAGGER_JSON ]]; then
-  sed -i "s|http://petstore.swagger.io/v2/swagger.json|/swagger.json|g" index.html
+if [[ -f $ABS_SWAGGER_JSON ]]; then
+  sed -i "s|http://petstore.swagger.io/v2/swagger.json|/$REL_SWAGGER_JSON|g" index.html
   sed -i "s|http://example.com/api|/swagger.json|g" index.html
 else
   sed -i "s|http://petstore.swagger.io/v2/swagger.json|$API_URL|g" index.html
